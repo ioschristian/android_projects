c00edc58c505e548114e77ffec080cf1642a27b0
Christian Tisby <christian.d.tisby@gmail.com>
2022-10-12 16:31:24 -0400

    completed section on lists and views in android

---
+++ WebView/app/src/main/java/com/christiantisby/webview/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.christiantisby.webview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("http://www.google.com")
+    }
+
+    override fun onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+
+    }
+}
